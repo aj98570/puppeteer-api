9a474ed25398f40ed07b9d46c7134dfa883f824e
aj98570 <ajay.yadav98570@gmail.com>
2025-06-19 00:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ app.get("/", async (req, res) => {
   }
 
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      executablePath,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
+const browser = await puppeteer.launch({
+  headless: "new", // or true
+  ignoreHTTPSErrors: true,
+  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+});
+
 
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
